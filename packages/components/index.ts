6bde5e105456095a3317fbe036f7e8c17381f231
赵琳阳 <zhaolinyang@tastien.com>
2025-09-05 16:50:07 +0800

    feat: 默认导出

--- packages/components/index.ts
+++ packages/components/index.ts
@@ -1,1 +1,4 @@
 export * from './src'
+import * as components from './src'
+const Tst = components
+export default Tst;
--- packages/components/src/index.ts
+++ packages/components/src/index.ts
@@ -1,4 +1,1 @@
 export * from './components'
-import * as components from './components'
-const Tst = components
-export default Tst;
